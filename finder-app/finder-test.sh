6f81db8829f1a596b069c41e3a0f3351be96eee7
Kashribha <shrinidhibhagavath11@gmail.com>
2026-02-09 17:11:58 +0530

    Forth - part2 assignment push

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+CONFIGPATH=/etc/finder-app/conf
+username=$(cat ${CONFIGPATH}/username.txt)
 
 #make clean
 #make
@@ -35,7 +36,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat ${CONFIGPATH}/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -57,10 +58,12 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+
+echo "$OUTPUTSTRING" > "/tmp/assignment4-result.txt"
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
